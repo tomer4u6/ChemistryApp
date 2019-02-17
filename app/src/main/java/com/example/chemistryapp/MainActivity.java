374ca8ef3629ee2df5bd0802e2d600cda02e47a4
tomer4u6 <tb9541@bs.amalnet.k12.il>
2019-02-17 13:30:59 +0200

    Version 1001

--- app/src/main/java/com/example/chemistryapp/MainActivity.java
+++ app/src/main/java/com/example/chemistryapp/MainActivity.java
@@ -1,29 +1,40 @@
-//Version 1000
+//Version 1001
 package com.example.chemistryapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn_tableActivity,btn_videosActivity,btn_mattersActivity,btn_alarmActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_tableActivity = (Button)findViewById(R.id.btn_tableActivity);
-        btn_videosActivity = (Button)findViewById(R.id.btn_videosActivity);
-        btn_mattersActivity = (Button)findViewById(R.id.btn_mattersActivity);
-        btn_alarmActivity = (Button)findViewById(R.id.btn_alarmActivity);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void onClick_tableActivity(View view) {
+        Intent t = new Intent(this,TableActivity.class);
+        startActivity(t);
+
+    }
+
+    public void onClick_videosActivity(View view) {
+    }
+
+    public void onClick_mattersActivity(View view) {
+    }
+
+    public void onClick_alarmActivity(View view) {
     }
 }
---
+++ app/src/main/java/com/example/chemistryapp/TableActivity.java
@@ -0,0 +1,27 @@
+package com.example.chemistryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_table);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
